9bb6b8ee941a78f5ce2676cde30cc2dfdea02e41
OleehyO <1258009915@qq.com>
2024-05-27 23:19:35 +0800

    bugfix: ocr_aug.py
    Change "lhy_custom" in ink_swap_color to "random"

--- src/models/ocr_model/utils/ocr_aug.py
+++ src/models/ocr_model/utils/ocr_aug.py
@@ -8,7 +8,7 @@ def ocr_augmentation_pipeline():
 
     ink_phase = [
         InkColorSwap(
-            ink_swap_color="lhy_custom",
+            ink_swap_color="random",
             ink_swap_sequence_number_range=(5, 10),
             ink_swap_min_width_range=(2, 3),
             ink_swap_max_width_range=(100, 120),
