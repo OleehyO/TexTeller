8bbae7360536d87266655c8e3c92efba68b08e3d
三洋三洋 <1258009915@qq.com>
2024-06-04 14:16:01 +0000

    Bugfix: to_katex.py

--- src/models/ocr_model/utils/to_katex.py
+++ src/models/ocr_model/utils/to_katex.py
@@ -54,7 +54,7 @@ def change(input_str, old_inst, new_inst, old_surr_l, old_surr_r, new_surr_l, ne
             result += input_str[i:start]
             i = start
     
-    if old_inst != new_inst and old_inst in result:
+    if old_inst != new_inst and (old_inst + old_surr_l) in result:
         return change(result, old_inst, new_inst, old_surr_l, old_surr_r, new_surr_l, new_surr_r)
     else:
         return result
@@ -63,6 +63,18 @@ def change(input_str, old_inst, new_inst, old_surr_l, old_surr_r, new_surr_l, ne
 def find_substring_positions(string, substring):
     positions = [match.start() for match in re.finditer(re.escape(substring), string)]
     return positions
+
+
+def rm_dollar_surr(content):
+    pattern = re.compile(r'\\[a-zA-Z]+\$.*?\$|\$.*?\$')
+    matches = pattern.findall(content)
+    
+    for match in matches:
+        if not re.match(r'\\[a-zA-Z]+', match):
+            new_match = match.strip('$')
+            content = content.replace(match, ' ' + new_match + ' ')
+    
+    return content
 
 
 def change_all(input_str, old_inst, new_inst, old_surr_l, old_surr_r, new_surr_l, new_surr_r):
@@ -76,8 +88,24 @@ def change_all(input_str, old_inst, new_inst, old_surr_l, old_surr_r, new_surr_l
 
 def to_katex(formula: str) -> str:
     res = formula
-    res = change_all(res, r'\mbox ', r'', r'{', r'}', r'', r'')
-    res = change_all(res, r'\mbox', r'', r'{', r'}', r'', r'')
+    # remove mbox surrounding
+    res = change_all(res, r'\mbox ', r' ', r'{', r'}', r'', r'')
+    res = change_all(res, r'\mbox', r' ', r'{', r'}', r'', r'')
+    # remove hbox surrounding
+    res = re.sub(r'\\hbox to ?-? ?\d+\.\d+(pt)?\{', r'\\hbox{', res)
+    res = change_all(res, r'\hbox', r' ', r'{', r'}', r'', r' ')
+    # remove raise surrounding
+    res = re.sub(r'\\raise ?-? ?\d+\.\d+(pt)?', r' ', res)
+    # remove makebox
+    res = re.sub(r'\\makebox ?\[\d+\.\d+(pt)?\]\{', r'\\makebox{', res)
+    res = change_all(res, r'\makebox', r' ', r'{', r'}', r'', r' ')
+    # remove vbox surrounding, scalebox surrounding
+    res = re.sub(r'\\raisebox\{-? ?\d+\.\d+(pt)?\}\{', r'\\raisebox{', res)
+    res = re.sub(r'\\scalebox\{-? ?\d+\.\d+(pt)?\}\{', r'\\scalebox{', res)
+    res = change_all(res, r'\scalebox', r' ', r'{', r'}', r'', r' ')
+    res = change_all(res, r'\raisebox', r' ', r'{', r'}', r'', r' ')
+    res = change_all(res, r'\vbox', r' ', r'{', r'}', r'', r' ')
+
 
     origin_instructions = [
         r'\Huge',
@@ -92,9 +120,13 @@ def to_katex(formula: str) -> str:
     ]
     for (old_ins, new_ins) in zip(origin_instructions, origin_instructions):
         res = change_all(res, old_ins, new_ins, r'$', r'$', '{', '}')
+    res = change_all(res, r'\boldmath ', r'\bm', r'{', r'}', r'{', r'}')
+    res = change_all(res, r'\boldmath', r'\bm', r'{', r'}', r'{', r'}')
     res = change_all(res, r'\boldmath ', r'\bm', r'$', r'$', r'{', r'}')
     res = change_all(res, r'\boldmath', r'\bm', r'$', r'$', r'{', r'}')
     res = change_all(res, r'\scriptsize', r'\scriptsize', r'$', r'$', r'{', r'}')
+    res = change_all(res, r'\emph', r'\textit', r'{', r'}', r'{', r'}')
+    res = change_all(res, r'\emph ', r'\textit', r'{', r'}', r'{', r'}')
     
     origin_instructions = [
         r'\left',
@@ -125,15 +157,30 @@ def to_katex(formula: str) -> str:
     if res.endswith(r'\newline'):
         res = res[:-8]
 
-    res = res.replace(r'\,', '')
-    res = res.replace(r'\!', '')
-    res = res.replace(r'\;', '')
-    res = res.replace(r'\:', '')
-    return res
+    # remove multiple spaces
+    res = re.sub(r'(\\,){1,}', ' ', res)
+    res = re.sub(r'(\\!){1,}', ' ', res)
+    res = re.sub(r'(\\;){1,}', ' ', res)
+    res = re.sub(r'(\\:){1,}', ' ', res)
+    res = re.sub(r'\\vspace\{.*?}', '', res)
+
+    # merge consecutive text
+    def merge_texts(match):
+        texts = match.group(0)
+        merged_content = ''.join(re.findall(r'\\text\{([^}]*)\}', texts))
+        return f'\\text{{{merged_content}}}'
+    res = re.sub(r'(\\text\{[^}]*\}\s*){2,}', merge_texts, res)
+
+    res = res.replace(r'\bf ', '')
+    res = rm_dollar_surr(res)
+
+    # remove extra spaces (keeping only one)
+    res = re.sub(r' +', ' ', res)
+
+    return res.strip()
 
 
 if __name__ == "__main__" :
-    test = r'''\[\mathsf{T}_{\infty}\,:=\,\big{(}\mathcal{X}\times\mathcal{W}\big{)}^{\infty}\]'''
-    # res = change_all(test, r'\boldmath', r'\bm', r'$', r'$', r'{', r'}')
+    test = r'''\[\textbf{a}(\textbf{\emph{i}},\textbf{\emph{j}})=(\textbf{\emph{a}}_{1}( \textbf{\emph{i}},\textbf{\emph{j}}),\textbf{\emph{a}}_{2}(\textbf{\emph{i}}, \textbf{\emph{j}}),\cdots,\textbf{\emph{a}}_{n}(\textbf{\emph{i}},\textbf{\emph {j}}))\]'''
     res = to_katex(test)
     print(res)
