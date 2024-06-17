d8659cd3a9a5b5b4c17173e98be21f69e1894eeb
三洋三洋 <1258009915@qq.com>
2024-06-17 21:03:08 +0800

    Add formula detection service

--- src/client_demo.py
+++ src/client_demo.py
@@ -1,10 +1,12 @@
 import requests
 
-url = "http://127.0.0.1:8000/predict"
+rec_server_url = "http://127.0.0.1:8000/frec"
+det_server_url = "http://127.0.0.1:8000/fdet"
 
 img_path = "/your/image/path/"
 with open(img_path, 'rb') as img:
     files = {'img': img}
-    response = requests.post(url, files=files)
+    response = requests.post(det_server_url, files=files)
+    # response = requests.post(rec_server_url, files=files)
 
 print(response.text)
